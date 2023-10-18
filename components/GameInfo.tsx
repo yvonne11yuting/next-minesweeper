d84391c64eb10d86cd81c67a16590eed194f403a
yvonne11yuting <yvonne11yuting@gmail.com>
2023-10-19 02:35:28 +0800

    feat: add how to play

--- components/GameInfo.tsx
+++ components/GameInfo.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { AlarmClock, Flag, RotateCcw } from "lucide-react"
+import { Flag, Info, Mouse, RotateCcw, Timer } from "lucide-react"
+import { GAME_CONTROL_TEXT } from "@/constants/minesweeperCostants";
 
 interface GameInfoProps {
     remainingFlags: number;
@@ -15,6 +16,7 @@ const GameInfo = ({
     resetGame
 }: GameInfoProps) => {
     const [recordTime, setRecordTime] = useState<number>(0);
+    const [showGuide, setShowGuide] = useState<boolean>(false);
 
     useEffect(() => {
         let timer: number | undefined;
@@ -35,18 +37,42 @@ const GameInfo = ({
     }, [startTimer, pauseTimer, recordTime]);
 
     return (
-        <div className="flex justify-between pb-2">
-            <span className="inline-flex gap-2">
+        <div className="flex justify-between items-baseline pb-1">
+            <span className="inline-flex gap-2 w-16">
                 <Flag color="#dc2626" />
                 <span data-testid="GAME_FLAGS_NUM">{remainingFlags}</span>
+            </span>
+            <span className="inline-flex gap-2 w-20">
+                <Timer color="#f59e0b" />
+                <span>{recordTime}</span>
             </span>
             <button onClick={resetGame}>
                 <RotateCcw color="#a855f7" />
             </button>
-            <span className="inline-flex gap-2 w-20">
-                <AlarmClock color="#f59e0b" />
-                <span>{recordTime}</span>
-            </span>
+            <div className="relative">
+                <button onClick={() => setShowGuide(!showGuide)}>
+                    <Info className="text-slate-200" />
+                </button>
+                {
+                    showGuide && (
+                        <div className="absolute left-0 bottom-0 w-96 p-5 ml-6 bg-slate-100 text-slate-900 -translate-x-full translate-y-full z-10 rounded bg-opacity-80">
+                            <h2 className="text-xl font-semibold text-lime-800">How To Play</h2>
+                            <ul className="flex flex-col gap-2 mt-4 leading-6 text-base text-slate-700">
+                                {
+                                    GAME_CONTROL_TEXT.map(({ text, icon }, idx) => (
+                                        <li key={idx} className="flex gap-2">
+                                            <span className="flex-none text-lime-600">{
+                                                icon === 'mouse' ? <Mouse /> : <Timer />
+                                            }</span>
+                                            <span>{text}</span>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
---
+++ constants/minesweeperCostants.ts
@@ -0,0 +1,13 @@
+export const GAME_CONTROL_TEXT = [{
+    text: 'Left-click a square to reveal it.',
+    icon: 'mouse'
+}, {
+    text: 'Right-click a square to flag it.',
+    icon: 'mouse'
+}, {
+    text: 'Double-click the left mouse button on a numbered square if the number of flagged squares around it matches the number.',
+    icon: 'mouse'
+}, {
+    text: 'There\'s a timer to record how quickly you complete the game.',
+    icon: 'timer'
+}];
