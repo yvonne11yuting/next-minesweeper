ad7f60e903367ae49f9913c19c771fb706173d35
yvonne11yuting <yvonne11yuting@gmail.com>
2023-10-20 15:26:17 +0800

    style: remove redundant semicolon

--- utils/domUtils.ts
+++ utils/domUtils.ts
@@ -1,6 +1,6 @@
 export const domUtils = {
     getDataSquare: (e: any): string => {
-        const SQUARE_ID = 'data-square';
-        return e.target?.closest(`[${SQUARE_ID}]`)?.getAttribute(SQUARE_ID) || '';
+        const SQUARE_ID = 'data-square'
+        return e.target?.closest(`[${SQUARE_ID}]`)?.getAttribute(SQUARE_ID) || ''
     }
 }
--- utils/minesweeperUtils.ts
+++ utils/minesweeperUtils.ts
@@ -10,27 +10,27 @@ export enum GameStatusEnum {
 }
 
 export class Minesweeper {
-    rows: number;
-    cols: number;
-    squareStatus: { [key: string]: string };
-    mines: string[];
-    gameStatus: GameStatusEnum = GameStatusEnum.INIT;
+    rows: number
+    cols: number
+    squareStatus: { [key: string]: string }
+    mines: string[]
+    gameStatus: GameStatusEnum = GameStatusEnum.INIT
 
     constructor(rows: number, cols: number, mines: string[], squareStatus: { [key: string]: string }) {
-        this.rows = rows;
-        this.cols = cols;
-        this.mines = mines;
-        this.squareStatus = squareStatus;
+        this.rows = rows
+        this.cols = cols
+        this.mines = mines
+        this.squareStatus = squareStatus
     }
 
     get generateBoard(): string[] {
-        let board: string[] = [];
+        let board: string[] = []
         for (let r = 0; r < this.rows; r++) {
             for (let c = 0; c < this.cols; c++) {
-                board.push(`${r}-${c}`);
+                board.push(`${r}-${c}`)
             }
         }
-        return board;
+        return board
     }
 
     private getAdjacentSquares(row: number, col: number): string[] {
@@ -43,73 +43,73 @@ export class Minesweeper {
             `${row + 1}-${col - 1}`, // left bottom
             `${row + 1}-${col}`,     // bottom
             `${row + 1}-${col + 1}`, // right bottom
-        ].filter(key => /^\d+-\d+$/.test(key));
+        ].filter(key => /^\d+-\d+$/.test(key))
     }
 
     private generateMine(): string {
-        const row = Math.floor(Math.random() * this.rows);
-        const col = Math.floor(Math.random() * this.cols);
-        return `${row}-${col}`;
+        const row = Math.floor(Math.random() * this.rows)
+        const col = Math.floor(Math.random() * this.cols)
+        return `${row}-${col}`
     }
 
     initMines(firstPosition: string, totalMines: number) {
-        const newMines: string[] = [];
+        const newMines: string[] = []
         while (newMines.length < totalMines) {
-            const key = this.generateMine();
+            const key = this.generateMine()
             if (!newMines.includes(key) && key !== firstPosition) {
-                newMines.push(key);
+                newMines.push(key)
             }
         }
-        this.mines = newMines;
-        this.gameStatus = GameStatusEnum.PLAYING;
+        this.mines = newMines
+        this.gameStatus = GameStatusEnum.PLAYING
     }
 
     checkSquare(squareId: string) {
         if (this.mines.includes(squareId)) {
-            this.gameStatus = GameStatusEnum.LOSE;
-            return;
+            this.gameStatus = GameStatusEnum.LOSE
+            return
         }
-        if (this.squareStatus[squareId]) return;
+        if (this.squareStatus[squareId]) return
 
-        const [row, col] = squareId.split('-').map(Number);
-        if (row < 0 || row >= this.rows || col < 0 || col >= this.cols) return;
+        const [row, col] = squareId.split('-').map(Number)
+        if (row < 0 || row >= this.rows || col < 0 || col >= this.cols) return
 
-        const adjacentSquares = this.getAdjacentSquares(row, col);
-        const minesAround = adjacentSquares.filter(key => this.mines.includes(key));
-        this.squareStatus = { ...this.squareStatus, [squareId]: minesAround.length.toString() };
-        if (minesAround.length > 0) return;
+        const adjacentSquares = this.getAdjacentSquares(row, col)
+        const minesAround = adjacentSquares.filter(key => this.mines.includes(key))
+        this.squareStatus = { ...this.squareStatus, [squareId]: minesAround.length.toString() }
+        if (minesAround.length > 0) return
 
         for (const targetSquare of adjacentSquares) {
-            this.checkSquare(targetSquare);
+            this.checkSquare(targetSquare)
         }
-        this.checkGameWin();
+        this.checkGameWin()
     }
 
     checkAdjacentSquares(squareId: string, flagged: string[]) {
-        const [row, col] = squareId.split('-').map(Number);
-        const adjacentSquares = this.getAdjacentSquares(row, col);
-        const adjacentFlags = adjacentSquares.filter(key => flagged.includes(key));
+        const [row, col] = squareId.split('-').map(Number)
+        const adjacentSquares = this.getAdjacentSquares(row, col)
+        const adjacentFlags = adjacentSquares.filter(key => flagged.includes(key))
         if (adjacentFlags.length === Number(this.squareStatus[squareId])) {
             for (let targetSquare of adjacentSquares) {
                 // by pass if square is flagged and is a mine
-                if (flagged.includes(targetSquare) && this.mines.includes(targetSquare)) continue;
-                this.checkSquare(targetSquare);
+                if (flagged.includes(targetSquare) && this.mines.includes(targetSquare)) continue
+                this.checkSquare(targetSquare)
             }
         }
     }
 
     checkGameWin = () => {
-        const totalOpened = Object.keys(this.squareStatus).length;
+        const totalOpened = Object.keys(this.squareStatus).length
         if (totalOpened + this.mines.length === this.rows * this.cols) {
-            this.gameStatus = GameStatusEnum.WIN;
-            return true;
+            this.gameStatus = GameStatusEnum.WIN
+            return true
         }
-        return false;
+        return false
     }
 
     resetGame() {
-        this.gameStatus = GameStatusEnum.INIT;
-        this.mines = [];
-        this.squareStatus = {};
+        this.gameStatus = GameStatusEnum.INIT
+        this.mines = []
+        this.squareStatus = {}
     }
 }
